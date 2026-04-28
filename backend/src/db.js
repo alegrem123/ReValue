23dd363a94fd4b94de6124a0287d32699edfdd9e
Alessandro <alessandro.turri9@gmail.com>
2026-04-28 10:49:40 +0200

    feat(db): add MongoDB connection module

---
+++ backend/src/db.js
@@ -0,0 +1,26 @@
+/**
+ * db.js
+ * Gestisce la connessione a MongoDB tramite mongojs.
+ * Esporta una singola istanza condivisa del DB.
+ */
+
+const mongojs = require('mongojs');
+require('dotenv').config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('MONGODB_URI non definita nel file .env');
+}
+
+const db = mongojs(MONGODB_URI);
+
+db.on('error', (err) => {
+  console.error('Errore connessione MongoDB:', err);
+});
+
+db.on('connect', () => {
+  console.log('Connesso a MongoDB Atlas');
+});
+
+module.exports = db;
